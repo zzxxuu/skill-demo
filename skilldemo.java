bd00e1126562c1484059e752729727bc5274447a
zzxxuu <100633138+zzxxuu@users.noreply.github.com>
2022-05-01 21:23:30 -0700

    A

--- skilldemo.java
+++ skilldemo.java
@@ -1,3 +1,9 @@
 public class skilldemo{
 
+    public static int subtract(int n, int m){
+        return n-m;
+    }
+
+
 }
+
---
+++ skilldemotest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skilldemotest {
+    
+    @Test
+    public void subtraction() {
+        assertEquals(2, skilldemo.subtract(3,2));
+    }
+}
